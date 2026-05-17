14b0493628f883c03d7a248acc31201c1752dd34
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-05-17 15:16:00 +0800

    Create android_ci_build.bash

---
+++ android_ci_build.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# set -e
+
+# git clone --depth 1 https://github.com/aaaapai/FastSTL.git ./include/FastSTL
+cmake_build () {
+  ANDROID_ABI=$1
+  mkdir -p build
+  cd build
+  cmake $GITHUB_WORKSPACE/webrtc-jni/src/main/cpp -DANDROID_PLATFORM=26 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake -DThreads_FOUND=ON -DCMAKE_THREAD_LIBS_INIT="-pthread" -DCMAKE_USE_PTHREADS_INIT=ON
+  cmake --build . --config Release --parallel 6
+  # 在bash中启用globstar
+  shopt -s globstar
+  $ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip $GITHUB_WORKSPACE/**/libMobileGL.so
+}
+
+cmake_build arm64-v8a
