bf9f91b91a5dec7dae6f3f1f1d19fa185608b3af
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-05-17 15:47:48 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -6,7 +6,7 @@ cmake_build () {
   ANDROID_ABI=$1
   mkdir -p build
   cd build
-  cmake $GITHUB_WORKSPACE/webrtc-jni/src/main/cpp -DANDROID_PLATFORM=26 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake -DThreads_FOUND=ON -DCMAKE_THREAD_LIBS_INIT="-pthread" -DCMAKE_USE_PTHREADS_INIT=ON
+  cmake $GITHUB_WORKSPACE/webrtc-jni/src/main/cpp -DWEBRTC_SRC_DIR=webrtc-jni/src/main/cpp/src -DANDROID_PLATFORM=26 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake -DThreads_FOUND=ON -DCMAKE_THREAD_LIBS_INIT="-pthread" -DCMAKE_USE_PTHREADS_INIT=ON
   cmake --build . --config Release --parallel 6
   # 在bash中启用globstar
   shopt -s globstar
